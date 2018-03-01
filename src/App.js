9e739183d8b1f49d1ad96ec4200fcbc60a182853
Elllie <bourgoishelene@gmail.com>
2018-03-01 11:01:27 +0100

    push app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Login from './components/Login/LoginSection'
+import Signup from './components/SignupForm/SignUp'
 import Recipe from './components/Recipes/RecipeComponent'
 import Profil from './components/Profils/Profil'
 import Home from './components/Home/Home'
-// import PrivateRoute from './components/Login/PrivateComponent'
 // import RecipeDetail from './components/Recipes/RecipeDetailComponent'
 import ReactDOM from 'react-dom'
 import { 
@@ -64,35 +64,38 @@ export const fakeAuth = {
     />
   )
 
-const App = appProps => (
-    <Router>
-    <nav className="navbar navbar-default">
-        <div className="App">
-        <div className="navbar-header">
-            <a className="navbar-brand" href="#">My react app</a>
+class App extends React.Component {
+   render() {
+    return (
+    <div>  
+      <nav className='navbar navbar-default'>
+        <div className='App'>
+          <div className='navbar-header'>
+              <a className='navbar-brand' href='#'>My react app</a>
+          </div>
+          <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+              <ul className='nav navbar-nav'>
+                  <li><Link to='/login'>Login</Link></li>
+                  <li><Link to='/signup'>Signup</Link></li>
+                  <li><Link to='/home'>Home</Link></li>
+                  <li><Link to='/recipes'>Recipes</Link></li>
+                  <li><Link to='/profil'>Profil</Link></li>
+              </ul>        
+          </div>
         </div>
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/recipes">Recipes</Link></li>
-                <li><Link to="/profil">Profil</Link></li>
-            </ul> 
-            <hr/>
-                
-        </div>
+      </nav> 
+          <Router>
             <Switch>
-                <Route path="/login" component={Login} />
-                <Route exact path="/home" component={Home} />
-                <PrivateRoute path="/recipes" component={Recipe} />
-                <Route path="/profil" component={Profil} />
+                <Route path='/login' component={Login} />
+                <Route path='/signup' component={Signup} />
+                <Route exact path='/home' component={Home} />
+                <PrivateRoute path='/recipes' component={Recipe} />
+                <Route path='/profil' component={Profil} />
             </Switch> 
-         </div>
-    </nav>     
-    </Router>    
-)      
+          </Router>  
+     </div>     
+    )
+   }  
+}     
     
-
-
-
 export default App
