92b78b711eac8e438c3a617c0c2a6b34b437d543
Elllie <bourgoishelene@gmail.com>
2018-03-12 18:17:31 +0100

    push recipeComponent

--- src/components/Recipes/RecipeComponent.js
+++ src/components/Recipes/RecipeComponent.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Route, Link, Redirect } from 'react-router-dom'
+import Cookies from 'universal-cookie'
+import { List } from 'semantic-ui-react'
+
 
 
 class Recipe extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            recipes: []
+            recipes: [],
+            recipeId: false
         }
     }
 
+
     componentDidMount() {
+        let cookies = new Cookies()
+        let userid = cookies.get('userid')
+         console.log('userid' + userid)
         let was = this
-        axios.get('http://localhost:3000/recipes')
+        axios.get('http://localhost:3000/recipes', {
+            params: {
+                _user: userid
+            }
+        })
         .then(function(response) {
-            console.log(response)
+            // console.log(response)
             was.setState({ recipes: response.data})
 
         })
@@ -28,16 +40,21 @@ class Recipe extends React.Component {
     render() {
 
         const { recipes } = this.state
-        //ici jextraie recipes de state, et lui donne la nouvelle valeur contenue dans response attention cest du ES6
-
+        const { recipeId } = this.state
+        if (recipeId) {
+            console.log(recipeId)
+            let url = '/foodDetail/' + recipeId
+            return <Redirect to={ url } push={ true } />
+        }
         return (
-           
-            <div>
+            <div role='list' className='ui link list'>
+            <a style={{cursor: 'pointer'}}>
                 {recipes.map(recipe =>
-                    <div key={recipe._id}>
-                        <ul><li>{recipe.name}</li></ul>   
+                    <div role='listitem' onClick={() => this.setState({ recipeId: recipe._id })} key={recipe._id}>
+                        <ul><li role='listitem' className='item'>{recipe.name}</li></ul>  
                     </div>
                 )}
+            </a>  
             </div>        
             
         )
